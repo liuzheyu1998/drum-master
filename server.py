83054ab157d62cc02602049224f10526f405d283
Haowei Cui <haowei.cui2000@gmail.com>
2022-04-16 15:29:15 -0400

    starting to include Yin's update

--- server.py
+++ server.py
@@ -3,32 +3,103 @@ from flask import render_template, redirect
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
-data = {
-    "1":{
-        "id": "1", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[7],
+# data = {
+#     "1":{
+#         "id": "1", 
+#         "audio": "/static/crash_cymbal.mp3", 
+#         "answer":[7],
 
+#     },
+#     "2":{
+#         "id": "2", 
+#         "audio": "/static/crash_cymbal.mp3", 
+#         "answer":[6]
+#     },
+#     "3":{
+#         "id": "3", 
+#         "audio": "/static/crash_cymbal.mp3", 
+#         "answer":[4]
+#     },
+#     "4":{
+#         "id": "4", 
+#         "audio": "/static/crash_cymbal.mp3", 
+#         "answer":[0,1,2,3]
+#     },
+#     "5":{
+#         "id": "5", 
+#         "audio": "/static/crash_cymbal.mp3", 
+#         "answer":[1,2,3]
+#     }
+# }
+
+drum_kit = {
+    "1": {
+        "id": "1",
+        "name": "hi_hat",
+        "audio_path": "/static/audio_drum/hi_hat.mov"
     },
-    "2":{
-        "id": "2", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[6]
+    "2": {
+        "id": "2",
+        "name": "crash1",
+        "audio_path": "/static/audio_drum/crash1.mov"
     },
-    "3":{
-        "id": "3", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[4]
+    "3": {
+        "id": "3",
+        "name": "crash2",
+        "audio_path": "/static/audio_drum/crash2.mov"
     },
-    "4":{
-        "id": "4", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[0,1,2,3]
+    "4": {
+        "id": "4",
+        "name": "snare_drum",
+        "audio_path": "/static/audio_drum/snare_drum.mov"
     },
-    "5":{
-        "id": "5", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[1,2,3]
+    "5": {
+        "id": "5",
+        "name": "hi_tom",
+        "audio_path": "/static/audio_drum/hi_tom.mov"
+    },
+    "6": {
+        "id": "6",
+        "name": "mid_tom",
+        "audio_path": "/static/audio_drum/mid_tom.mov"
+    },
+    "7": {
+        "id": "7",
+        "name": "low_tom",
+        "audio_path": "/static/audio_drum/low_tom.mov"
+    },
+    "8": {
+        "id": "8",
+        "name": "bass_drum",
+        "audio_path": "/static/audio_drum/bass_drum.mov"
+    }
+}
+
+quiz = {
+    "1": {
+        "id": "1",
+        "audio_path": "/static/audio_quiz/quiz1.mp3",
+        "answer": [4]
+    },
+    "2": {
+        "id": "2",
+        "audio_path": "/static/audio_quiz/quiz2.mp3",
+        "answer": [3]
+    },
+    "3": {
+        "id": "3",
+        "audio_path": "/static/audio_quiz/quiz3.mp3",
+        "answer": [1, 1, 4, 1]
+    },
+    "4": {
+        "id": "4",
+        "audio_path": "/static/audio_quiz/quiz4.mp3",
+        "answer": [8, 4, 8, 8, 4]
+    },
+    "5": {
+        "id": "5",
+        "audio_path": "/static/audio_quiz/quiz5.mp3",
+        "answer": [4, 4, 5, 5, 6, 6, 7, 7, 2]
     }
 }
 score = {"score":0}
@@ -63,7 +134,8 @@ def quiz(id=None):
         answer.clear()
     
 
-    return render_template('quiz.html', data=data[id], score=score) 
+    #return render_template('quiz.html', data=data[id], score=score) 
+    return render_template('quiz.html', drum_kit = drum_kit) 
 
 @app.route('/quizresult/<id>')
 def quizresult(id=None):
--- static/quiz.js
+++ static/quiz.js
@@ -1,136 +1,144 @@
 let arr = []
-function playQuestion() {
-    var audio = new Audio('/static/crash_cymbal.mp3');
-    audio.play();
+
+function play() {
+    let id = event.srcElement.id
+    console.log("played " + drum_kit[id]["name"] + " (id=" + id + ")")
+    let audio_path = new Audio(drum_kit[id]["audio_path"]);
+    audio_path.play();
 }
-function play1() {
-    var audio = new Audio('/static/crash_cymbal.mp3');
-    arr.push(0)
+
+// function playQuestion() {
+//     var audio = new Audio('/static/crash_cymbal.mp3');
+//     audio.play();
+// }
+// function play1() {
+//     var audio = new Audio('/static/crash_cymbal.mp3');
+//     arr.push(0)
     
-    audio.play();
-    console.log(arr)
-}
-function play2() {
-    var audio = new Audio('/static/crash_cymbal.mp3');
-    arr.push(1)
-    audio.play();
-    console.log(arr)
-}
-function play3() {
-    var audio = new Audio('/static/crash_cymbal.mp3');
-    arr.push(2)
-    audio.play();
-    console.log(arr)
-}
-function play4() {
-    var audio = new Audio('/static/crash_cymbal.mp3');
-    arr.push(3)
-    audio.play();
-    console.log(arr)
-}
-function play5() {
-    var audio = new Audio('/static/crash_cymbal.mp3');
-    arr.push(4)
-    audio.play();
-    console.log(arr)
-}
-function play6() {
-    var audio = new Audio('/static/crash_cymbal.mp3');
-    arr.push(5)
-    audio.play();
-    console.log(arr)
-}
-function play7() {
-    var audio = new Audio('/static/crash_cymbal.mp3');
-    arr.push(6)
-    audio.play();
-    console.log(arr)
-}
-function play8() {
-    var audio = new Audio('/static/crash_cymbal.mp3');
-    arr.push(7)
-    audio.play();
-    console.log(arr)
-}
-function arraysEqual(a1,a2) {
-    return JSON.stringify(a1)==JSON.stringify(a2);
-}
+//     audio.play();
+//     console.log(arr)
+// }
+// function play2() {
+//     var audio = new Audio('/static/crash_cymbal.mp3');
+//     arr.push(1)
+//     audio.play();
+//     console.log(arr)
+// }
+// function play3() {
+//     var audio = new Audio('/static/crash_cymbal.mp3');
+//     arr.push(2)
+//     audio.play();
+//     console.log(arr)
+// }
+// function play4() {
+//     var audio = new Audio('/static/crash_cymbal.mp3');
+//     arr.push(3)
+//     audio.play();
+//     console.log(arr)
+// }
+// function play5() {
+//     var audio = new Audio('/static/crash_cymbal.mp3');
+//     arr.push(4)
+//     audio.play();
+//     console.log(arr)
+// }
+// function play6() {
+//     var audio = new Audio('/static/crash_cymbal.mp3');
+//     arr.push(5)
+//     audio.play();
+//     console.log(arr)
+// }
+// function play7() {
+//     var audio = new Audio('/static/crash_cymbal.mp3');
+//     arr.push(6)
+//     audio.play();
+//     console.log(arr)
+// }
+// function play8() {
+//     var audio = new Audio('/static/crash_cymbal.mp3');
+//     arr.push(7)
+//     audio.play();
+//     console.log(arr)
+// }
+// function arraysEqual(a1,a2) {
+//     return JSON.stringify(a1)==JSON.stringify(a2);
+// }
 
-function submit(cur_score){
-    let data_to_save = {
-        "score": cur_score,
-        "id":data["id"],
-        "correct":false,
-        "answer":""
-    }
-    data_to_save["answer"] = arr
-    // if (arraysEqual(arr, data["answer"])){
-    //     data_to_save["score"] += 1
-    //     data_to_save["correct"] = "1"
-    // }
-    // else{
-    //     data_to_save["correct"] = "0"
-    // }
+// function submit(cur_score){
+//     let data_to_save = {
+//         "score": cur_score,
+//         "id":data["id"],
+//         "correct":false,
+//         "answer":""
+//     }
+//     data_to_save["answer"] = arr
+//     // if (arraysEqual(arr, data["answer"])){
+//     //     data_to_save["score"] += 1
+//     //     data_to_save["correct"] = "1"
+//     // }
+//     // else{
+//     //     data_to_save["correct"] = "0"
+//     // }
 
-    id = parseInt(data["id"])
-    new_url = "/quizresult/"+id.toString()
-    let next_id = id+1
-    $.ajax({
-        type: "POST",
-        url: "/submitAnswer",                
-        dataType : "json",
-        contentType: "application/json; charset=utf-8",
-        data : JSON.stringify(data_to_save),
-        success: function(result){
+//     id = parseInt(data["id"])
+//     new_url = "/quizresult/"+id.toString()
+//     let next_id = id+1
+//     $.ajax({
+//         type: "POST",
+//         url: "/submitAnswer",                
+//         dataType : "json",
+//         contentType: "application/json; charset=utf-8",
+//         data : JSON.stringify(data_to_save),
+//         success: function(result){
             
-            if(next_id <= 5){
-                let next_url = "/quiz/"+next_id
-                window.location.href=next_url
+//             if(next_id <= 5){
+//                 let next_url = "/quiz/"+next_id
+//                 window.location.href=next_url
     
-            }
-            else{
-                window.location.href="/quizfeedback"
-            }
+//             }
+//             else{
+//                 window.location.href="/quizfeedback"
+//             }
             
 
-        },
-        error: function(request, status, error){
-            console.log("There is an Error");
-            console.log(request)
-            console.log(status)
-            console.log(error)
-        }
-    });
+//         },
+//         error: function(request, status, error){
+//             console.log("There is an Error");
+//             console.log(request)
+//             console.log(status)
+//             console.log(error)
+//         }
+//     });
 
 
         
 
 
-}
+//}
 
 $(document).ready(function(){
-    cur_score = score["score"]
-    console.log(cur_score)
-    let questionTitle = "Question "+data["id"]+": Please replicate the tempo"
+    // cur_score = score["score"]
+    // console.log(cur_score)
+    // let questionTitle = "Question "+data["id"]+": Please replicate the tempo"
     
-    console.log(questionTitle)
-    $("#questionTitle").html(questionTitle)
+    // console.log(questionTitle)
+    // $("#questionTitle").html(questionTitle)
     
     
-    $("#submit").click(function(){   
-        console.log(cur_score)
-        console.log("here")
+    // $("#submit").click(function(){   
+    //     console.log(cur_score)
+    //     console.log("here")
                    
-        submit(cur_score)
+    //     submit(cur_score)
 
         
-    })
-    $("#clear").click(function(){   
-        arr=[]
-        console.log(arr)
+    // })
+    // $("#clear").click(function(){   
+    //     arr=[]
+    //     console.log(arr)
 
         
-    })
+    // })
 
 
    
