fbcff717693bf8999d661554118dfd87803d9180
Yin <57527292+jyincheng@users.noreply.github.com>
2022-04-15 23:45:48 -0400

    fix audio and drum kit
    1. trim audio to reduce sound latency
    2. fix sounds of bass drum and low tom

--- server.py
+++ server.py
@@ -3,74 +3,46 @@ from flask import render_template, redirect
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
-data = {
-    "1":{
-        "id": "1", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[1,2,3]
-    },
-    "2":{
-        "id": "2", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[1,2,3]
-    },
-    "3":{
-        "id": "3", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[1,2,3]
-    },
-    "4":{
-        "id": "4", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[1,2,3]
-    },
-    "5":{
-        "id": "5", 
-        "audio": "/static/crash_cymbal.mp3", 
-        "answer":[1,2,3]
-    }
-}
-
 drum_kit = {
     "1": {
         "id": "1",
         "name": "hi_hat",
-        "audio_path": "/static/audio_drum/hi_hat.mp3"
+        "audio_path": "/static/audio_drum/hi_hat.mov"
     },
     "2": {
         "id": "2",
         "name": "crash1",
-        "audio_path": "/static/audio_drum/crash1.mp3"
+        "audio_path": "/static/audio_drum/crash1.mov"
     },
     "3": {
         "id": "3",
         "name": "crash2",
-        "audio_path": "/static/audio_drum/crash2.mp3"
+        "audio_path": "/static/audio_drum/crash2.mov"
     },
     "4": {
         "id": "4",
         "name": "snare_drum",
-        "audio_path": "/static/audio_drum/snare_drum.mp3"
+        "audio_path": "/static/audio_drum/snare_drum.mov"
     },
     "5": {
         "id": "5",
         "name": "hi_tom",
-        "audio_path": "/static/audio_drum/hi_tom.mp3"
+        "audio_path": "/static/audio_drum/hi_tom.mov"
     },
     "6": {
         "id": "6",
         "name": "mid_tom",
-        "audio_path": "/static/audio_drum/mid_tom.mp3"
+        "audio_path": "/static/audio_drum/mid_tom.mov"
     },
     "7": {
         "id": "7",
         "name": "low_tom",
-        "audio_path": "/static/audio_drum/low_tom.mp3"
+        "audio_path": "/static/audio_drum/low_tom.mov"
     },
     "8": {
         "id": "8",
         "name": "bass_drum",
-        "audio_path": "/static/audio_drum/bass_drum.mp3"
+        "audio_path": "/static/audio_drum/bass_drum.mov"
     }
 }
 
