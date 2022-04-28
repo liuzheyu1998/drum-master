6e1722467a6deae6c75ca9b078cf94ba1a42011f
Yin <57527292+jyincheng@users.noreply.github.com>
2022-04-28 11:35:20 -0400

    change button size, font, feedback styling

--- static/quizFeedback.js
+++ static/quizFeedback.js
@@ -6,42 +6,42 @@ $(document).ready(function(){
     feedback = "Congratulations, you got " + score +"/5!"
     $("#feedback").html(feedback)
     if (isCorrect["1"] == "1"){
-        $("#q1-cor").html("Correct")
+        $("#q1-cor").html("&#10004")
     }
     else{
-        $("#q1-cor").html("Wrong")
+        $("#q1-cor").html("&#10060")
         $("#q1-cor").addClass("bold")
     }
     $("#feedback").html(feedback)
     if (isCorrect["2"] == "1"){
-        $("#q2-cor").html("Correct")
+        $("#q2-cor").html("&#10004")
     }
     else{
-        $("#q2-cor").html("Wrong")
+        $("#q2-cor").html("&#10060")
         $("#q2-cor").addClass("bold")
     }
     $("#feedback").html(feedback)
     if (isCorrect["3"] == "1"){
-        $("#q3-cor").html("Correct")
+        $("#q3-cor").html("&#10004")
     }
     else{
-        $("#q3-cor").html("Wrong")
+        $("#q3-cor").html("&#10060")
         $("#q3-cor").addClass("bold")
     }
     $("#feedback").html(feedback)
     if (isCorrect["4"] == "1"){
-        $("#q4-cor").html("Correct")
+        $("#q4-cor").html("&#10004")
     }
     else{
-        $("#q4-cor").html("Wrong")
+        $("#q4-cor").html("&#10060")
         $("#q4-cor").addClass("bold")
     }
     $("#feedback").html(feedback)
     if (isCorrect["5"] == "1"){
-        $("#q5-cor").html("Correct")
+        $("#q5-cor").html("&#10004")
     }
     else{
-        $("#q5-cor").html("Wrong")
+        $("#q5-cor").html("&#10060")
         $("#q5-cor").addClass("bold")
     }
     $("#relearn").click(function(){ 
